e686f278c9ab4b71a62036da3795487e26d8498e
Unchaptered <workstation19961002@gmail.com>
2022-08-11 14:55:07 +0900

    Update `app.js`

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const app = express();
 
 app.listen(() => {
     console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
 });
